769d1dc42448a980cd882f8052fbe011c29742a1
Archibald Pontier <archibald.pontier@phd.codes>
2016-03-16 21:16:54 +0100

    Preparing display for countdown

--- clock.ml
+++ clock.ml
@@ -7,9 +7,6 @@ let delay = Some 1.0
 let wx = ref 0
 let wy = ref 0
 
-let draw (x,y) =
-    I.(string A.(fg green) square |> hpad x 0 |> vpad y 0)
-
 let sign n =
     if n > 0 then 1 else if n < 0 then -1 else 0
 
@@ -18,24 +15,40 @@ let rec makelist n v =
     | 0 -> []
     | a -> v :: (makelist (a - 1) v)
 
-let hline n x y =
-    let s = String.concat "" (makelist n square) in
-    I.(string A.(fg green) s |> hpad x 0 |> vpad y 0)
+module Primitives : sig
+    val draw : int -> int -> Notty.image
+    val draw_text : string -> int -> int -> Notty.image
+    val hline : int -> int -> int -> Notty.image
+    val vline : int -> int -> int -> Notty.image
+end = struct
+    let draw x y =
+        I.(string A.(fg green) square |> hpad x 0 |> vpad y 0)
 
-let vline n x y =
-    let rec s_vline n x y =
-        if n > 1 then
-            I.(s_vline (n - 1) x y <-> string A.(fg green) square)
-        else I.(string A.(fg green) square)
-    in
-    I.(s_vline n x y |> hpad x 0 |> vpad y 0)
+    let draw_text s x y =
+        I.(string A.(fg green) s |> hpad x 0 |> vpad y 0)
+
+    let hline n x y =
+        let s = String.concat "" (makelist n square) in
+        I.(string A.(fg green) s |> hpad x 0 |> vpad y 0)
+
+    let vline n x y =
+        let rec s_vline n x y =
+            if n > 1 then
+                I.(s_vline (n - 1) x y <-> string A.(fg green) square)
+            else I.(string A.(fg green) square)
+        in
+        I.(s_vline n x y |> hpad x 0 |> vpad y 0)
+end
 
 module Numbers : sig
     type t
     val numbers : int -> t
     val to_img : int -> int -> t -> Notty.image
 end = struct
+    open Primitives
+
     type t = Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Nine | Err | Dots
+
     let numbers = function
         | 0 -> Zero
         | 1 -> One
@@ -56,24 +69,91 @@ end = struct
         | Two       -> I.(hline 3 x y </> vline 4 (x + 3) y </> hline 4 x (y + 3) </> vline 4 x (y + 3) </> hline 4 x (y + 6))
         | Three     -> I.(hline 3 x y </> vline 7 (x + 3) y </> hline 3 x (y + 3) </> hline 3 x (y + 6))
         | Four      -> I.(vline 4 x y </> vline 7 (x + 3) y </> hline 3 x (y + 3))
-        | Five       -> I.(hline 4 x y </> vline 4 x y </> hline 3 x (y + 3) </> vline 4 (x + 3) (y + 3) </> hline 4 x (y + 6))
-        | Six      -> I.(hline 4 x y </> vline 7 x y </> vline 4 (x + 3) (y + 3) </> hline 4 x (y+3) </> hline 4 x (y + 6))
+        | Five      -> I.(hline 4 x y </> vline 4 x y </> hline 3 x (y + 3) </> vline 4 (x + 3) (y + 3) </> hline 4 x (y + 6))
+        | Six       -> I.(hline 4 x y </> vline 7 x y </> vline 4 (x + 3) (y + 3) </> hline 4 x (y+3) </> hline 4 x (y + 6))
         | Seven     -> I.(hline 4 x y </> vline 7 (x + 3) y)
         | Eight     -> I.(hline 4 x y </> vline 7 x y </> vline 7 (x + 3) y </> hline 4 x (y + 3) </> hline 4 x (y + 6))
         | Nine      -> I.(hline 4 x y </> vline 3 x y </> hline 4 x (y + 3) </> vline 7 (x + 3) y)
-        | Dots      -> I.(draw (x + 1, y + 3) </> draw (x + 1, y + 6))
+        | Dots      -> I.(draw (x + 1) (y + 3) </> draw (x + 1) (y + 6))
         | Err       -> I.(hline 4 x (y + 3))
 end
+
+type clock = {
+    mutable x: int;
+    mutable y: int;
+}
+
+let init_clock term =
+    let wx, wy = Term.size term in
+    { x = (wx - 38)/2; y = (wy - 7)/2; }
+
+let update_clock c w h =
+    c.x <- (w - 38)/2;
+    c.y <- (h - 7)/2;
+
+type ctstatus = Off | Writing | On
+
+type countdown = {
+    mutable status: ctstatus;
+    mutable start: int;
+    mutable target: int;
+    mutable wtarget: int list;
+}
+
+let format_h n =
+    let h = n/3600 in
+    let m = (n - 3600*h)/60 in
+    let s = n mod 60 in
+    (h, m, s)
+
+let unformat_h (h, m, s) = h*3600 + m*60 + s
+
+let init_countdown () = { status = Off; start = 0; target = 0; wtarget = []; }
+
+let trigger_countdown ct =
+    let t = int_of_float (Unix.time ()) in
+    ct.status <- On;
+    ct.start <- t;
+    ct.target <- t + ct.target
+
+let rec ( ** ) n = function
+    | 0 -> 1
+    | m -> if m < 0 then 1 else n * (( ** ) n (m - 1))
+
+let write_countdown ct =
+    let rec wc l p = match l with
+        | []    -> 0
+        | n::ll -> n * (10 ** p) + wc ll (p + 1)
+    in
+    let (h,m,s) = format_h (wc ct.wtarget 1) in
+    (string_of_int h) ^ ":" ^ (string_of_int m) ^ ":" ^ (string_of_int s)
+
+let display_status t ct x y =
+    let open Primitives in
+    match ct.status with
+    | Off -> I.(hline 28 x y </> draw_text "00:00:00" (x + 29) y)
+    | Writing -> I.(hline 28 x y </> draw_text (write_countdown ct) (x + 29) y)
+    | On -> let cur = (int_of_float t) - ct.target in
+            let (h, m, s) = format_h cur in
+            let l = ((float_of_int ct.start) -. t) /. (float_of_int (ct.start - ct.target)) in
+            let n_s = int_of_float (28.0 *. l) in
+            let ts = String.concat "" [string_of_int h; ":"; string_of_int m; ":"; string_of_int s] in
+            I.(hline n_s x y </> draw_text ts (x + 29) y)
+
+let update_status ct x y = match ct.status with
+    | Off -> display_status (-1.0) ct x y
+    | Writing -> display_status (-1.0) ct x y
+    | On ->
+            let t = (Unix.time ()) in
+            if ct.target < (int_of_float t) then ct.status <- Off;
+            display_status t ct x y
 
 let display_time x y =
     let t = Unix.localtime (Unix.time ()) in
     let h, m, s = (t.Unix.tm_hour, t.Unix.tm_min, t.Unix.tm_sec) in
-    let h10 = h/10 in
-    let h1 = h mod 10 in
-    let m10 = m/10 in
-    let m1 = m mod 10 in 
-    let s10 = s/10 in
-    let s1 = s mod 10 in
+    let h10, h1 = h/10, h mod 10 in
+    let m10, m1 = m/10, m mod 10 in 
+    let s10, s1 = s/10, s mod 10 in
     I.(Numbers.(to_img x y (numbers h10) </> to_img (x + 5) y (numbers h1) </> to_img (x + 10) y (numbers (-1)) </> to_img (x + 14) y (numbers m10) </> to_img (x + 19) y (numbers m1) </> to_img (x + 24) y (numbers (-1)) </> to_img (x + 28) y (numbers s10) </> to_img (x + 33) y (numbers s1)))
 
 let timer () = Lwt_unix.sleep 1.0 >|= fun _ -> `Timer
@@ -81,25 +161,27 @@ let event term = Lwt_stream.get (Term.events term) >|= function
     | Some (`Resize _ | #Unescape.event as x) -> x
     | None -> `End
 
-let rec loop term (e, t) =
+let render term c ct =
+    let ic = display_time c.x c.y in
+    let ict = update_status ct c.x (c.y + 8) in
+    Term.image term I.(ic </> ict)
+
+let rec loop term c ct (e, t) =
     (e <?> t) >>= function
         | `End | `Key (`Enter, _) -> Lwt.return_unit
         | `Timer ->
-                let i = display_time ((!wx - 38)/2) ((!wy - 7)/2) in
-                Term.image term i >>= fun () -> loop term (e, timer ())
+            render term c ct >>= fun () -> loop term c ct (e, timer ())
         | `Resize dim ->
-                let twx, twy = Term.size term in
-                wx := twx; wy := twy;
-                let i = display_time ((!wx - 38)/2) ((!wy - 7)/2) in
-                Term.image term i >>= fun () -> loop term (event term, timer ())
-        | _ -> loop term (event term, timer ())
+            let w, h = Term.size term in
+            c.x <- w; c.y <- h;
+            render term c ct >>= fun () -> loop term c ct (event term, timer ())
+        | _ -> loop term c ct (event term, timer ())
 
 let main () =
     let term = Term.create() in
-    let twx, twy = Term.size term in
-    wx := twx; wy := twy;
-    let i = display_time ((!wx - 38)/2) ((!wy - 7)/2) in
-    Term.image term i >>= fun () ->
-        loop term (event term, timer ())
+    let c = init_clock term in
+    let ct = init_countdown () in
+    render term c ct  >>= fun () ->
+        loop term c ct (event term, timer ())
 
 let () = Lwt_main.run @@ main ()
